cc7c2ca504a137d294d88a31f9f53b3d0b5d52c6
p.vasilev <p.vasilev@g.nsu.ru>
2022-07-16 07:04:34 +0700

    Add test environment with testcontainers

--- src/test/kotlin/ru/jarsoft/test/JarsoftTestApplicationTests.kt
+++ src/test/kotlin/ru/jarsoft/test/JarsoftTestApplicationTests.kt
@@ -2,9 +2,32 @@ package ru.jarsoft.test
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.utility.DockerImageName
+
 
 @SpringBootTest
+@Testcontainers
+@ActiveProfiles("testcontainers")
 class JarsoftTestApplicationTests {
+
+    companion object {
+
+        @Container
+        val redis: GenericContainer<*> =
+            GenericContainer(DockerImageName.parse("redis:5.0.3-alpine"))
+                .withExposedPorts(6379)
+
+        init {
+            redis.start()
+            System.setProperty("spring.redis.host", redis.host)
+            System.setProperty("spring.redis.port", redis.getMappedPort(6379).toString())
+        }
+
+    }
 
     @Test
     fun contextLoads() {
